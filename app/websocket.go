bc228a65f161a97ba420932705c7a6b87f3a87df
SteveOC64 <steveoc64@gmail.com>
2016-02-02 05:47:04 +1030

    bidirectional rpc

--- app/websocket.go
+++ app/websocket.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	// "github.com/gopherjs/gopherjs/js"
+	"bufio"
+	"encoding/gob"
+	// "errors"
 	"github.com/gopherjs/websocket"
 	"honnef.co/go/js/dom"
+	"io"
 	"net/rpc"
 )
 
@@ -29,13 +33,61 @@ func websocketInit() *websocket.Conn {
 		print("failed to open websocket")
 	}
 	ws = wss
-	rpcClient = rpc.NewClient(ws)
+
+	encBuf := bufio.NewWriter(ws)
+	client := &myClientCodec{ws, gob.NewDecoder(ws), gob.NewEncoder(encBuf), encBuf}
+	rpcClient = rpc.NewClientWithCodec(client)
 
 	// Now we can spawn a pinger against the backetd
 	go sendPings(55000)
 
-	// And run a server at this end that accepts pings
 	//go PingServer(ws)
 
 	return wss
+}
+
+// codec to encode requests and decode responses.''
+
+type myClientCodec struct {
+	rwc    io.ReadWriteCloser
+	dec    *gob.Decoder
+	enc    *gob.Encoder
+	encBuf *bufio.Writer
+}
+
+func (c *myClientCodec) WriteRequest(r *rpc.Request, body interface{}) (err error) {
+	print("rpc writes a new header", r)
+	if err = c.enc.Encode(r); err != nil {
+		return
+	}
+	if err = c.enc.Encode(body); err != nil {
+		return
+	}
+	return c.encBuf.Flush()
+}
+
+func (c *myClientCodec) ReadResponseHeader(r *rpc.Response) error {
+	err := c.dec.Decode(r)
+	if err != nil {
+		print("rpc error", err.Error())
+		return err
+	}
+	if r.ServiceMethod[:1] == "*" {
+		print("Async update from server", r.ServiceMethod)
+		return nil
+		// return errors.New("Async update from server - NOT an RPC call")
+	}
+	return err
+}
+
+func (c *myClientCodec) ReadResponseBody(body interface{}) error {
+	print("rpc reads message body")
+	err := c.dec.Decode(body)
+	print("rpc gets body", body)
+	return err
+}
+
+func (c *myClientCodec) Close() error {
+	print("calling close")
+	return c.rwc.Close()
 }
--- server/websocket.go
+++ server/websocket.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"github.com/labstack/echo"
 	"golang.org/x/net/websocket"
+	"log"
 	"net/rpc"
+	"time"
 )
 
 type ConnectionsList struct {
@@ -40,6 +43,27 @@ func webSocket(c *echo.Context) error {
 	ws.PayloadType = websocket.BinaryFrame
 
 	Connections.Add(ws).Show("Connections Grows To:")
+	go sendPings(ws, 10000)
 	rpc.ServeConn(ws)
 	return nil
+}
+
+// Constantly Ping the Backend
+func sendPings(ws *websocket.Conn, ms time.Duration) {
+
+	ticker := time.NewTicker(time.Millisecond * ms)
+	enc := gob.NewEncoder(ws)
+	for _ = range ticker.C {
+		r := rpc.Response{}
+		r.ServiceMethod = "*Ping"
+		log.Println("sending ping to client", r)
+		if err := enc.Encode(&r); err != nil {
+			log.Println("Some sort of error sending Ping header", err.Error())
+			return
+		}
+		payload := "an example payload"
+		enc.Encode(&payload)
+		// send an empty body
+
+	}
 }
