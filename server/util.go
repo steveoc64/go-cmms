a733878e9bdeeda1ca9ce853858722dcc3cb6a25
SteveOC64 <steveoc64@gmail.com>
2016-04-25 04:40:14 +0930

    patch partclass on machines and parts

--- server/util.go
+++ server/util.go
@@ -136,7 +136,7 @@ func (u *UtilRPC) Machine(channel int, result *string) error {
 		*result = r
 	}
 
-	logger(start, "Util.Backup",
+	logger(start, "Util.Machine",
 		fmt.Sprintf("Channel %d, User %d %s %s",
 			channel, conn.UserID, conn.Username, conn.UserRole),
 		*result)
@@ -151,16 +151,51 @@ func (u *UtilRPC) Parts(channel int, result *string) error {
 	conn := Connections.Get(channel)
 
 	if conn.UserRole == "Admin" && conn.Username == "steve" {
-		out, err := exec.Command("ls", "-ltra").Output()
-		if err != nil {
-			log.Println(err)
-			return nil
+		// For each part, get the 1st component that its associated
+		// with (under the old scheme), and from there get the machine.
+		//
+		// The machine then tells us which partclass to use
+		r := "Processing Parts\n"
+
+		parts := []shared.Part{}
+
+		DB.SQL(`select * from part
+			where class=0
+			order by id`).
+			QueryStructs(&parts)
+
+		patched := 0
+		for _, p := range parts {
+			r += fmt.Sprintf("Part %d: %s", p.ID, p.Name)
+
+			// Get the first associated component
+			classID := 0
+			err := DB.SQL(`select 
+				m.part_class
+				from component_part x
+				left join component c on c.id=x.component_id
+				left join machine m on m.id=c.machine_id
+				where x.part_id=$1
+				limit 1`, p.ID).QueryScalar(&classID)
+
+			if err != nil {
+				r += fmt.Sprintf("\n    !! No can find the partclass\n")
+				continue
+			}
+
+			if classID == 0 {
+			} else {
+				r += fmt.Sprintf(" = PartClass %d\n", classID)
+				DB.SQL(`update part set class=$1 where id=$2`, classID, p.ID).Exec()
+				patched++
+			}
 		}
-		// log.Println("Result =", out)
-		*result = fmt.Sprintf("The ls is %s\n", out)
+
+		r += fmt.Sprintf("\nPatched %d of %d Parts\n", patched, len(parts))
+		*result = r
 	}
 
-	logger(start, "Util.Backup",
+	logger(start, "Util.Parts",
 		fmt.Sprintf("Channel %d, User %d %s %s",
 			channel, conn.UserID, conn.Username, conn.UserRole),
 		*result)
